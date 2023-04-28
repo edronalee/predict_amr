ab8add4391e0ee798edc2d11e618767ac6c1ad64
unknown <ljtavu@up.edu.ph>
2023-04-28 18:31:10 +0800

    fix the navbar link

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'dashboard'
 
 urlpatterns = [
-    path('', views.about, name="about"),
+    path('', views.about, name='about'),
+    path('demo/', views.demo, name='demo'),
+    path('antibiogram/', views.antibiogram, name='antibiogram'),
+    path('modeldetails/', views.modeldetails, name='modeldetails'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 def about(request):
     return render(request, 'about.html', {})
+
+def demo(request):
+    return render(request, 'demo.html', {})
+
+def antibiogram(request):
+    return render(request, 'antibiogram.html', {})
+
+def modeldetails(request):
+    return render(request, 'modeldetails.html', {})
