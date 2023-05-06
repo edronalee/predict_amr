34be03582e5ae50894fa99a8f502c24a9a3e395e
unknown <ljtavu@up.edu.ph>
2023-05-06 16:29:01 +0800

    fix the fasta file upload

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class FastaUploadForm(forms.Form):
-    fasta_file = forms.FileField(label='Upload Fasta File', widget=forms.ClearableFileInput(attrs={'multiple': True}))
+    fasta_file = forms.FileField(label='Upload Fasta File', widget=forms.ClearableFileInput(attrs={'multiple': True, 'class':'form-control'}))
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from dashboard.forms import *
 
+import os
+from django.conf import settings
+
 # Create your views here.
 def about(request):
     return render(request, 'about.html', {})
@@ -13,14 +16,42 @@ def antibiogram(request):
     if request.method == 'POST':
         form = FastaUploadForm(request.POST, request.FILES)
         if form.is_valid():
-            # process the fasta file and return the predictions
-            form = FastaUploadForm()
-            print('hello')
-        else:
-            form = FastaUploadForm()
+            # Save the uploaded file to the local machine
+            uploaded_file = request.FILES['fasta_file']
+            filename = handle_uploaded_file(uploaded_file)
+            
+            print('testinggggg')
+            # Run IntegronFinder and Abricate subprocesses on the saved file
+            # integron_output = subprocess.check_output(['integron_finder', '-i', filename])
+            # abricate_output = subprocess.check_output(['abricate', filename])
+            
+            # Process the output data and return the predictions
+            # predictions = process_output(integron_output, abricate_output)
+            
+            # Render the template with the predictions
+            # return render(request, 'prediction.html', {'predictions': predictions})
     return render(request, 'antibiogram.html', {'form': form})
-
-
 
 def modeldetails(request):
     return render(request, 'modeldetails.html', {})
+
+def handle_uploaded_file(uploaded_file):
+    """
+    Save the uploaded file to the MEDIA_ROOT directory with a unique name.
+    Return the filename.
+    """
+    filename = uploaded_file.name
+    file_path = os.path.join(settings.MEDIA_ROOT, filename)
+    
+    # Check if a file with the same name already exists
+    #if os.path.exists(file_path):
+        # If the file exists, add a suffix to the filename to make it unique
+    #    filename = f'{os.path.splitext(filename)[0]}_{uuid.uuid4().hex}{os.path.splitext(filename)[1]}'
+    #    file_path = os.path.join(settings.MEDIA_ROOT, filename)
+    
+    # Write the uploaded file data to the new file
+    with open(file_path, 'wb+') as destination:
+        for chunk in uploaded_file.chunks():
+            destination.write(chunk)
+    
+    return filename
